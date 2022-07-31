35601a690ef354fc4d9dc18156d309205c679407
Anna Everson <aeverson@zendesk.com>
2022-07-30 21:31:04 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "events#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/events", to: "events#index"
+  get "/events/:id", to: "events#show"
 end
